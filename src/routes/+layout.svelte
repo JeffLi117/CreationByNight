9289e18a2a07643d769a209451be137a6373d291
JeffLi117 <li.jeffreyt@yahoo.com>
2023-05-05 11:22:17 -0500

    Fixed layouts for separate pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,1 @@
-<script> 
-    import '../app.css';
-    import Nav from '$lib/nav.svelte';
-    import Fa from 'svelte-fa/src/fa.svelte'
-    import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
-</script>
-
-<Nav />
-<main>
-    <!-- page  content -->
-    <slot></slot>
-</main>
-<footer>
-    <p>Copyright 2023 <a href="https://github.com/JeffLi117">JeffLi117 <Fa icon={faGithub} /></a></p>
-</footer>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,30 @@
-<script>
-
+<script> 
+    import '../app.css';
+    import Fa from 'svelte-fa/src/fa.svelte'
+    import { faGithub } from '@fortawesome/free-brands-svg-icons';
 </script>
 
 <h1>CreationsByNight</h1>
 <p>Anonymously artistic.</p>
 <p>Creatively crafty.</p>
 <p>I hope you like what you see.</p>
+<nav>
+    <ul class="ulHome" >
+        <li><a href="/">Home</a></li>
+        <li><a href="/portfolio">Portfolio</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+</nav>
 
+<footer>
+    <p>Copyright 2023 <a href="https://github.com/JeffLi117">JeffLi117 <Fa icon={faGithub} /></a></p>
+</footer>
 <style>
-    
+    nav, ul.ulHome {
+        text-align: left;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        padding: 0;
+    }
 </style>
